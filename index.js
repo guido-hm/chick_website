4f222d56194e4fc27854bbdb66d95c6a8772032c
Guido Herrera <guidohm@tamu.edu>
2022-12-06 23:41:38 -0600

    fixed logged issue by replacing atob

--- index.js
+++ index.js
@@ -63,16 +63,23 @@ app.get('/', (req, res) => {
 function decodeJwtResponse(token) {
     var base64Url = token.split(".")[1];
     var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    var jsonPayload = decodeURIComponent(
-    atob(base64)
-        .split("")
-        .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join("")
-    );
 
-    return JSON.parse(jsonPayload);
+    var buf = Buffer.from(base64, 'base64').toString('utf-8'); // Ta-da
+    console.log("DECODING BUF: ")
+    console.log(buf);
+
+
+
+    // var jsonPayload = decodeURIComponent(
+    // atob(base64)
+    //     .split("")
+    //     .map(function (c) {
+    //     return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+    //     })
+    //     .join("")
+    // );
+
+    return JSON.parse(buf);
 }
 
 
